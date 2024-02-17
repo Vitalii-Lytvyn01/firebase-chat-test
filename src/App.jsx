81322974037ad1ba27dfaebffc98980e980576a2
Vitalii Lytvyn <vitalii.lytvyn01@gmail.com>
2024-02-17 17:38:47 +0200

    eod commit: room id input added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import './App.css';
+import './App.scss';
 import { Auth } from './components/Auth';
 import Cookies from 'universal-cookie';
 
@@ -7,15 +7,53 @@ const cookies = new Cookies();
 
 function App() {
   const [isAuth,setIsAuth] = useState(cookies.get(('auth-token')));
-  const [roomId,setRoomId] = useState(null);
+  const [selectedRoomId, setSelectedRoomId] = useState(null);
+  const roomId = useRef(null);
 
   const roomInputRef = useRef(null);
 
-  return (
-    <>
-    <Auth setIsAuth={setIsAuth}/>
-    </>
-  )
+  function handleRoomInput(e) {
+    roomInputRef.current = e.target.value;
+  }
+
+  function handleRoomSelect() {
+    console.log(roomInputRef.current);
+    setSelectedRoomId(roomInputRef.current);
+  }
+
+  if(!setIsAuth) {
+    return (
+      <>
+      <Auth setIsAuth={setIsAuth}/>
+      </>
+    )
+  }
+
+  return <>
+    {
+      !selectedRoomId ?
+        <div className="room-select">
+          <p>
+            Input room ID
+          </p>
+          <input
+            value={roomId.current}
+            onChange={(e) => handleRoomInput(e)}
+            type="text"
+            name="room-input"
+            id="room-input"
+            className='room-input'
+            />
+            <div
+              className="room-button"
+              onClick={handleRoomSelect}
+            >Select</div>
+        </div>
+      :
+      "Chat"
+    }
+  </>
+
 }
 
 export default App
